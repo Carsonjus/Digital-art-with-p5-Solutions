639bc4011cb456a44c7500999dfbd90d4d656e2b
Carsonjus <42914118+Carsonjus@users.noreply.github.com>
2023-07-15 14:54:08 -0500

    Create 1.2.6 Ellipse.js

---
+++ Module 1.2 Solutions/1.2.6 Ellipse.js
@@ -0,0 +1,12 @@
+function setup(){
+    // Set up your drawing here
+    createCanvas(500,500);
+    background("beige");
+    
+    //fill(color(0,0,0)); //This is optional but maybe challenge if we are moving through this really fast
+    circle(300,200,400);
+}
+
+function draw() {
+    // Write your drawing code below
+}
