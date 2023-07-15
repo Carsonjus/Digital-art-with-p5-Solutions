941d9f477944063fd8abf23c654ce6de8f783aa2
Carsonjus <42914118+Carsonjus@users.noreply.github.com>
2023-07-15 14:43:14 -0500

    Create 1.2.5 Rectangle.js

---
+++ Module 1.2 Solutions/1.2.5 Rectangle.js
@@ -0,0 +1,18 @@
+function setup(){
+    // Set up your drawing here
+    createCanvas(500,500);
+    //background("red"); //If the students are having a hard time seeing what they are doing advise them to change the background color to help visualize it
+    
+    //Create the squares
+    rect(0,0,260,340);
+    rect(260,0,500,160);
+    rect(0,340,150,160);
+    rect(150,340,110,160);
+    rect(260,160,240,340);
+    
+    //Most important for the students to know this isn't drawing a box w/ 4 coordinates, instead setting the top left corner than the width and height
+}
+
+function draw() {
+    // Write your drawing code below
+}
