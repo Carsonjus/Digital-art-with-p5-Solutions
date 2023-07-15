d3c0239a5912d1e5266d8400e860829bf8ecf566
Carsonjus <42914118+Carsonjus@users.noreply.github.com>
2023-07-15 14:29:43 -0500

    Create 1.2.4 Line.js

---
+++ Module 1.2 Solutions/1.2.4 Line.js
@@ -0,0 +1,11 @@
+function setup(){
+    // Set up your drawing here
+    createCanvas(500, 500);
+    background("beige");
+    line(100, 0, 500, 500);
+    line(500, 0, 100, 500);
+}
+
+function draw() {
+    // Write your drawing code below
+}
