de12244f429bf308bb149a8bc937202af666c8c8
Carsonjus <42914118+Carsonjus@users.noreply.github.com>
2023-07-15 19:37:26 -0500

    Create 1.4.3 Making Variables.js

---
+++ Module 1.4 Solutions/1.4.3 Making Variables.js
@@ -0,0 +1,14 @@
+// Write your variables here
+function setup(){
+    createCanvas(500,400);
+    var red = 100;
+    var green = 32;
+    var blue = 80;
+    
+    background(red,green,blue);
+    
+}
+
+function draw() {
+
+}
